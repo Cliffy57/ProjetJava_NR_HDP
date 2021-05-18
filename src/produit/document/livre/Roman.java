2882ba45597e781564bd44965195eb2a6a66b2ff
ooolsa <rbtnicolas57@gmail.com>
2021-05-18 14:48:07 +0200

    ajout classe Roman

---
+++ src/produit/document/livre/Roman.java
@@ -0,0 +1,12 @@
+package produit.document.livre;
+
+import produit.document.Livre;
+
+public class Roman extends Livre {
+
+    public Roman(double id, String titre, float tarif) {
+        super(id, titre, tarif);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
