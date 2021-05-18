c6a64257eea6a696897e40d1af447a1da345c77d
ooolsa <rbtnicolas57@gmail.com>
2021-05-18 14:48:53 +0200

    ajout classe ManuelScolaire

---
+++ src/produit/document/livre/ManuelScolaire.java
@@ -0,0 +1,12 @@
+package produit.document.livre;
+
+import produit.document.Livre;
+
+public class ManuelScolaire extends Livre {
+
+    public ManuelScolaire(double id, String titre, float tarif) {
+        super(id, titre, tarif);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
