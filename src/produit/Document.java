2e424e70e3bef849cb336161e8304578ae37be15
ooolsa <rbtnicolas57@gmail.com>
2021-05-18 14:46:46 +0200

    ajout classe Document

---
+++ src/produit/Document.java
@@ -0,0 +1,10 @@
+package produit;
+
+public class Document extends Produit {
+
+    public Document(double id, String titre, float tarif) {
+        super(id, titre, tarif);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
