d754fd5599d90901264d6a88e5c8c16e32c9a403
ooolsa <rbtnicolas57@gmail.com>
2021-05-28 11:22:26 +0200

    création classe emprunt

---
+++ src/projet/produit/Emprunt.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Emprunt {
+    private Date dateDeb;
+    private Date dateFin;
+    private ArrayList<Produit> listeProduits;
+
+    Emprunt(Date dateDeb, Date dateFin) {
+        this.dateDeb = dateDeb;
+        this.dateFin = dateFin;
+        this.listeProduits = new ArrayList<Produit>();
+    }
+
+    public Date getDateDeb() {
+        return dateDeb;
+    }
+
+    public Date getDateFin() {
+        return dateFin;
+    }
+
+    public ArrayList<Produit> getListeProduits() {
+        return listeProduits;
+    }
+
+    public void addProduit(Produit produit) {
+        this.listeProduits.add(produit);
+    }
+}
