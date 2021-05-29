634a40e516ab6ad3330b17a2957b12df2bcd6b3d
ooolsa <rbtnicolas57@gmail.com>
2021-05-29 15:52:07 +0200

    ajout doc Client

--- app/src/main/java/projetjava/client/Client.java
+++ app/src/main/java/projetjava/client/Client.java
@@ -8,33 +8,13 @@ public abstract class Client {
 	private String prenom;
 	private float reduction;
 	ArrayList<Commande> listeCommande;
-	
-	public int getIdentifiant() {
-		return identifiant;
-	}
-	public void setIdentifiant(int identifiant) {
-		this.identifiant = identifiant;
-	}
-	public String getNom() {
-		return this.nom;
-	}
-	
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public String getPrenom() {
-		return prenom;
-	}
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-	public float getReduction() {
-		return reduction;
-	}
-	public void setReduction(float reduction) {
-		this.reduction = reduction;
-	}
-	
+
+	/**
+	 * crée un objet Client
+	 * @param identifiant
+	 * @param nom
+	 * @param prenom
+	 */
 	
 	public Client(int identifiant, String nom, String prenom) {
 		super();
@@ -44,9 +24,92 @@ public abstract class Client {
 		this.listeCommande = new ArrayList<Commande>();
 	}
 	
+	/**
+	 * renvoie l'identifiant
+	 * @return identifiant
+	 */
+
+	public int getIdentifiant() {
+		return identifiant;
+	}
+
+	/**
+	 * définis un identifiant
+	 * @param identifiant
+	 */
+
+	public void setIdentifiant(int identifiant) {
+		this.identifiant = identifiant;
+	}
+
+	/**
+	 * renvoie un nom
+	 * @return nom
+	 */
+
+	public String getNom() {
+		return this.nom;
+	}
+
+	/**
+	 * définis le nom
+	 * @param nom
+	 */
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	/**
+	 * renvoie le prénom
+	 * @return prenom
+	 */
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	/**
+	 * définis le prénom
+	 * @param prenom
+	 */
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	/**
+	 * renvoie la réduction
+	 * @return réduction
+	 */
+
+	public float getReduction() {
+		return reduction;
+	}
+
+	/**
+	 * définis la réduction
+	 * @param reduction
+	 */
+
+	public void setReduction(float reduction) {
+		this.reduction = reduction;
+	}
+	
+	/**
+	 * renvoie la liste des commandes
+	 * @return liste des commandes
+	 */
+	
 	public ArrayList<Commande> getCommandeList() {
 	    return this.listeCommande;
 	  }
+
+	  /**
+	   * ajoute une commande a la liste
+	   * @param commande
+	   */
+
 	public void addCommande(Commande commande) {
 	    this.listeCommande.add(commande);
 	  
