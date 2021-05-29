92766777cf9fa8e62c457090eba9f310d895fbf0
ooolsa <rbtnicolas57@gmail.com>
2021-05-29 15:40:55 +0200

    ajout doc Emprunt

--- app/src/main/java/projetjava/client/Emprunt.java
+++ app/src/main/java/projetjava/client/Emprunt.java
@@ -11,23 +11,53 @@ public class Emprunt {
     private Calendar dateFin;
     private ArrayList<Produit> listeProduits;
 
+    /**
+     * Crée un objet Emprunt
+     * @param dateDeb date de début de l'emprunt 
+     * @param dateFin date de fin de l'emprunt
+     */
+
     public Emprunt(Calendar dateDeb, Calendar dateFin) {
         this.dateDeb = dateDeb;
         this.dateFin = dateFin;
         this.listeProduits = new ArrayList<Produit>();
     }
 
+    /**
+     * renvoie la date de début de l'objet
+     * @return la date de début
+     * @see Calendar
+     */
+
     public Calendar getDateDeb() {
         return dateDeb;
     }
+
+    /**
+     * renvoie ladate de fin de l'objet
+     * @return une date
+     * @see Calendar
+     */
 
     public Calendar getDateFin() {
         return dateFin;
     }
 
+    /**
+     * renvoie la liste des produits de l'objets
+     * @return une liste de produit
+     * @see Produit
+     */
+
     public ArrayList<Produit> getListeProduits() {
         return listeProduits;
     }
+
+    /**
+     * ajoute un produit a la liste de produits
+     * @param produit un produit a ajouter a la liste
+     * @see Produit
+     */
 
     public void addProduit(Produit produit) {
         this.listeProduits.add(produit);
